49e2ecac489909420559bfe570efcafb6a6be69f
AmeyKokane <amey.kokane@gmail.com>
2024-06-17 16:27:17 -0500

    Delete Day2__Data_Types_StringsManipulation.py

--- Day2__Data_Types_StringsManipulation.py
+++
@@ -1,9 +0,0 @@
-# print("Hello")
-
-
-#Number of Characters in your Name
-num_char = len(input("Enter Your Name"))
-
-new_num_char = str(num_char)
-
-print("Your name has " + new_num_char + " characters.")
